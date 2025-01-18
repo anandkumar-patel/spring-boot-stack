e7860c4dd3adfdedbd5981eeb147407ebcbb8ffa
anandkumar-patel <kumar.akpatel@gmail.com>
2025-01-18 21:17:11 +0530

    sys change

--- kafa-integration/src/main/java/anand/controller/KafkaController.java
+++ kafa-integration/src/main/java/anand/controller/KafkaController.java
@@ -14,7 +14,7 @@ public class KafkaController {
     private MessageProducer messageProducer;
 
     @PostMapping("/send")
-    public String sendMessage(@RequestParam("message") String message) {
+    public String sendMessage(@RequestParam String message) {
         messageProducer.sendMessage("my-topic", message);
         return "Message sent: " + message;
     }
